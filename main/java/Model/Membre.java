8cb16ead5c45519b3fa89948e2cafe0b8e9cca39
ZOUHAYR BAHOUMI <bahoumizouhayr@gmail.com>
2024-04-18 19:10:35 +0100

    ajouter la class javabean

---
+++ main/java/Model/Membre.java
@@ -0,0 +1,64 @@
+package Model;
+
+public class Membre {
+	private int id_membre;
+	private String nom;
+	private String prenom;
+	private String email;
+	private String phone;
+	private String cin;
+	private String adresse;
+	
+	
+	
+	
+	
+	public Membre() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public int getId_membre() {
+		return id_membre;
+	}
+	public void setId_membre(int id_membre) {
+		this.id_membre = id_membre;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getCin() {
+		return cin;
+	}
+	public void setCin(String cin) {
+		this.cin = cin;
+	}
+	public String getAdresse() {
+		return adresse;
+	}
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+	
+	
+}
