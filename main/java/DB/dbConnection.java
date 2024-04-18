6bd92cd72ce17643b2489b8ca6a71ad286823127
ZOUHAYR BAHOUMI <bahoumizouhayr@gmail.com>
2024-04-18 18:10:23 +0100

    ajouter les fichier du projet

---
+++ main/java/DB/dbConnection.java
@@ -0,0 +1,25 @@
+package DB;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class dbConnection {
+	private static final String URL = "jdbc:postgresql://localhost:5432/pharmacie";
+    private static final String USER = "postgres";
+    private static final String PASS = "sanaazbadi";
+
+    public static Connection getConnection() {
+    Connection connection = null;
+    try {
+
+        Class.forName("org.postgresql.Driver");
+
+        connection = DriverManager.getConnection(URL, USER, PASS);
+
+    } catch (SQLException | ClassNotFoundException e) {
+        System.err.println("Erreur lors de la connexion à la base de données : " + e.getMessage());
+    }
+    return connection;
+}
+}
