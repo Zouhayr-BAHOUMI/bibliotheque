30a23e2fb57c900501463ca5691df3aa0c4132e3
ZOUHAYR BAHOUMI <bahoumizouhayr@gmail.com>
2024-04-19 17:39:58 +0100

    la fonction ajouter et afficher

--- main/java/DB/dbMembre/dbConnection.java
+++ main/java/DB/dbMembre/dbConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class dbConnection {
-	private static final String URL = "jdbc:postgresql://localhost:5432/pharmacie";
+	private static final String URL = "jdbc:postgresql://localhost:5432/bibliotheque";
     private static final String USER = "postgres";
     private static final String PASS = "sanaazbadi";
 
--- main/java/Dao/Membre/MembreDao.java
+++ main/java/Dao/Membre/MembreDao.java
@@ -1,7 +1,14 @@
 package Dao.Membre;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 import DB.dbMembre.dbConnection;
+import Model.Membre;
 
 public class MembreDao {
 	
@@ -9,5 +16,48 @@ public class MembreDao {
     
     public MembreDao(){
         connect = dbConnection.getConnection();
+    }
+    
+    public void ajouterMembre(Membre membre) {
+        try {
+            PreparedStatement prstmt = connect.prepareStatement("INSERT INTO Membre (nom, prenom, email, phone, cin, adresse)"
+                    + " VALUES (?, ?, ?, ?, ?, ?)");
+            prstmt.setString(1, membre.getNom());
+            prstmt.setString(2, membre.getPrenom());
+            prstmt.setString(3, membre.getEmail());
+            prstmt.setString(4, membre.getPhone());
+            prstmt.setString(5, membre.getCin());
+            prstmt.setString(6, membre.getAdresse());
+            prstmt.executeQuery();
+        } catch (SQLException e) {
+        	e.printStackTrace();
+        }
+    } 
+    
+    
+    public List<Membre> afficherProduits(){
+        List<Membre> membres = new ArrayList<>();
+        try{
+            PreparedStatement prstmt = connect.prepareStatement("SELECT * from Membre");
+            ResultSet res = prstmt.executeQuery();
+            
+            while(res.next()){
+            	Membre membre = new Membre();
+                
+            	membre.setId_membre(res.getInt("id_membre"));
+            	membre.setNom(res.getString("nom"));
+            	membre.setPrenom(res.getString("prenom"));
+            	membre.setEmail(res.getString("email"));
+            	membre.setPhone(res.getString("phone"));
+            	membre.setCin(res.getString("cin"));
+            	membre.setAdresse(res.getString("adresse"));
+
+            	membres.add(membre);
+            }
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+        
+        return membres;
     }
 }
